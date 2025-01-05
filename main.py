d29ab6184d2e73d4ed79b1d43f78c74f8d60abee
tbirddv <terracb1997@gmail.com>
2025-01-04 22:05:25 -0500

    spins

--- main.py
+++ main.py
@@ -1,28 +1,25 @@
 import pygame
 from constants import *
-from circleshape import CircleShape
 from player import Player
 
 def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill(000000)
+        
+        screen.fill("black")
         player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
-        clock.tick(60)
+        
         dt = clock.tick(60) / 1000
-          
-
 
 if __name__ == "__main__":
     main()
